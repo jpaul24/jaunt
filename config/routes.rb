b4693454dcb419a6ce0cfe1d7169651bc940c030
Joe Paul <joe.paul0712@gmail.com>
2019-08-27 10:51:30 +0100

    sorting the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources: :activities
+  resources: :trips do
+    resources: :reviews, only: :create
+  end
+  resources: :reviews, only: [:edit, :update, :destroy]
+  resources: :users do
+
+  end
+  resources: :shortlisted_activities
+  resources: :activity_categories
 end
