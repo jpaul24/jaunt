b1134b4da97256c56ed20f15ef81bafe0e5c2a54
Terri Hounsom <hounsom.terri@gmail.com>
2019-08-27 14:15:51 +0100

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_152354) do
+ActiveRecord::Schema.define(version: 2019_08_27_125350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_152354) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
@@ -57,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_152354) do
     t.bigint "trip_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "day"
     t.index ["activity_id"], name: "index_shortlisted_activities_on_activity_id"
     t.index ["trip_id"], name: "index_shortlisted_activities_on_trip_id"
   end
@@ -67,6 +69,8 @@ ActiveRecord::Schema.define(version: 2019_08_26_152354) do
     t.integer "likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
+    t.integer "duration"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
@@ -78,6 +82,11 @@ ActiveRecord::Schema.define(version: 2019_08_26_152354) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "second_name"
+    t.string "username"
+    t.string "nationality"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
