095895fe769de78dd36ef705877f4b3f60e08aed
Nicholas Morrissey <n.mozza@hotmail.com>
2019-08-29 16:14:59 +0100

    config development environment changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.assets.initialize_on_precompile = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
