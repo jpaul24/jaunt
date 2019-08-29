247467d5a93efe15a9f6dd4842b7ce8cac29f6e5
Nicholas Morrissey <n.mozza@hotmail.com>
2019-08-29 15:52:27 +0100

    LOGIC FINALLY DONE

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :shortlisted_activities, dependent: :destroy
-
+  has_many :activities, through: :shortlisted_activities
   validates :duration, presence: true
   validates :duration, numericality: true
   validates :trip_name, length: { in: 5..50 }, on: :update
@@ -10,23 +10,48 @@ class Trip < ApplicationRecord
 
   def activities_hash
     trip_hash = Hash[trip_duration.map { |day| [day, { "Morning" => nil, "Afternoon" => nil, "Evening" => nil}] }]
-    shortlisted_activities.map { |shortlisted_activity| shortlisted_activity.activity }
-
+    shortlisted_activities.each do |shortlisted_activity|
+      if trip_hash[shortlisted_activity.day.to_i][shortlisted_activity.activity.tod].nil?
+        trip_hash[shortlisted_activity.day.to_i][shortlisted_activity.activity.tod] = shortlisted_activity.activity.name
+      end
+    end
+    trip_hash
   end
 
   def morning_activities
-    ShortlistedActivity.where(trip: self).joins(:activity).where(activities: {tod: 'Morning'})
+    morning = ShortlistedActivity.where(trip: self).joins(:activity).where(activities: {tod: 'Morning'})
+    day_increment(morning)
   end
 
   def afternoon_activities
-    ShortlistedActivity.where(trip: self).joins(:activity).where(activities: {tod: 'Afternoon'})
+    afternoon = ShortlistedActivity.where(trip: self).joins(:activity).where(activities: {tod: 'Afternoon'})
+    day_increment(afternoon)
   end
 
   def evening_activities
-    ShortlistedActivity.where(trip: self).joins(:activity).where(activities: {tod: 'Evening'})
+    evening = ShortlistedActivity.where(trip: self).joins(:activity).where(activities: {tod: 'Evening'})
+    day_increment(evening)
+  end
+
+  def sort_activities
+    morning_activities
+    afternoon_activities
+    evening_activities
   end
 
   def trip_duration
     (1..duration).to_a
+  end
+
+  def day_increment(activities)
+    activities.each_with_index do |activity, i|
+      if i < duration
+        activity.day = i + 1
+        activity.save
+      else
+        activity.day = duration
+        activity.save
+      end
+    end
   end
 end
