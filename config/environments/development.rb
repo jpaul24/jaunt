2145dadc7838ce10031078e6927ebcf77b6c4361
Nicholas Morrissey <n.mozza@hotmail.com>
2019-08-29 16:31:40 +0100

    deleted config lines

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,6 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-  config.assets.initialize_on_precompile = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
