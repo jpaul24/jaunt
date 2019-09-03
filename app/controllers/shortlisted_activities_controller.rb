755ae402da5ff97bd5fcb163976c04f89355e302
Terri Hounsom <hounsom.terri@gmail.com>
2019-09-03 15:43:25 +0100

    changed the if statement on the activities index

--- app/controllers/shortlisted_activities_controller.rb
+++ app/controllers/shortlisted_activities_controller.rb
@@ -4,13 +4,13 @@ class ShortlistedActivitiesController < ApplicationController
     @activity = Activity.find(@shortlisted_activity.activity_id)
     @trip = Trip.find(params[:trip_id])
 
-    @activity_map = Activity.geocoded
-    @activity_geocoded = @activity_map.find(params[:id])
+    # @activity_map = Activity.geocoded
+    # @activity_geocoded = @activity_map.find(params[:id])
     @markers =
     [
       {
-        lat: @activity_geocoded.latitude,
-        lng: @activity_geocoded.longitude
+        lat: @shortlisted_activity.activity.latitude,
+        lng: @shortlisted_activity.activity.longitude
       }
     ]
     @review = Review.new
