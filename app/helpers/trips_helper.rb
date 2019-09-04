8ddfd142f24aedc0dc5ae4a45b7e89793238793a
Nicholas Morrissey <n.mozza@hotmail.com>
2019-09-04 09:56:47 +0100

    logic fix to do with auto populating the days

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -7,13 +7,21 @@ module TripsHelper
     end
   end
 
-  def tod_icon_picker(i)
-    if i == 0
+  def tod_icon_picker(activity)
+    if activity.activity.tod == "Morning"
       "sunrise.png"
-    elsif i == 1
+    elsif activity.activity.tod == "Afternoon"
       "sun.png"
-    elsif i == 2
+    elsif activity.activity.tod == "Evening"
       "sunset.png"
     end
+  end
+
+  def tod_activity_picker(activities, day)
+    activities = activities.where(day: day)
+    morning = activities.find { |activity| activity.activity.tod == "Morning" }
+    afternoon = activities.find { |activity| activity.activity.tod == "Afternoon" }
+    evening = activities.find { |activity| activity.activity.tod == "Evening" }
+    [morning, afternoon, evening]
   end
 end
